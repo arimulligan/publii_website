440c5751637c4df3582f48fc1ae2d98e87da6f37
Ari <90117279+arimulligan@users.noreply.github.com>
2025-07-30 18:15:21 +0200

    Updated from Publii

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
    const openButtons = document.querySelectorAll('.js-open-video');
    const modal = document.getElementById('video-modal');
-   const iframe = document.getElementById('video-frame');
+   const videoFrame = document.getElementById('video-frame');
+   const transcriptFrame = document.getElementById('transcript-frame');
    const title = document.getElementById('video-title');
 
    openButtons.forEach(button => {
@@ -10,11 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
          const videoUrl = button.getAttribute('data-video');
          if (!videoUrl || videoUrl == "") {
             title.textContent = "Missing video link. Try again, or contact us if issue persists."
-            iframe.style.display = "none";
+            videoFrame.style.display = "none";
          } else {
             const videoTitle = button.getAttribute('data-title');
+            const transcript = button.getAttribute('data-transcript');
+            if (!transcript || transcript == "") {
+               transcriptFrame.style.display = "none";
+            } else {
+               transcriptFrame.src = transcript;
+            }
             title.textContent = videoTitle;
-            iframe.src = videoUrl.replace("watch?v=", "embed/") + "?autoplay=1";
+            videoFrame.src = videoUrl.replace("watch?v=", "embed/") + "?autoplay=1";
          }
          modal.style.display = "flex";
       });
@@ -23,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
    const closeButtons = [document.getElementById('video-close'), document.getElementById('video-close-btn')];
    closeButtons.forEach(el => {
       el.addEventListener('click', function () {
-         iframe.src = "";
+         videoFrame.src = "";
          modal.style.display = "none";
       });
    });
